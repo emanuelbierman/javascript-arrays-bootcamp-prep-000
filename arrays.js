1c6e6fcf97a90850935678bedd86da615c0897af
Emanuel Bierman <emanuel.bierman@gmail.com>
2017-11-01 15:09:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -54,3 +54,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift(0)
+}
+
